40c8a325b79ce447b8968cc177ed3c66e7f21962
Paolo Carollo <pcarollo84@gmail.com>
2018-11-10 16:29:27 +0000

    Initial Commit

---
+++ SilverlightContainer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SilverlightContainer
+//
+//  Created by Paolo Carollo on 10/11/2018.
+//  Copyright © 2018 Paolo Carollo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SilverlightContainer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SilverlightContainer
+//
+//  Created by Paolo Carollo on 10/11/2018.
+//  Copyright © 2018 Paolo Carollo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
